f08c6612c6633b12dfb5ebab8fb5e7ce236d869d
Abdullah al mamun <61002570+stranger-1-1-13@users.noreply.github.com>
2024-07-12 21:56:47 +0600

    loop and function

--- basic.php
+++ basic.php
@@ -10,22 +10,89 @@
 //     echo "not divisible by either 2 or 3";
 // }
 
-$level = 4;
-switch($level){
-    case 1:
-        echo "Your are playing at easy level";
-        break;
-    case 2:
-        echo "Your are playing at Moderate level";
-        break;
-    case 3:
-        echo "Your are playing at hard level";
-        break;
-    case 4:
-        echo "Your are playing at extreme level";
-        break;
-    case 5:
-    default:
-        echo "Invalid level";
+// $level = 4;
+// switch($level){
+//     case 1:
+//         echo "Your are playing at easy level";
+//         break;
+//     case 2:
+//         echo "Your are playing at Moderate level";
+//         break;
+//     case 3:
+//         echo "Your are playing at hard level";
+//         break;
+//     case 4:
+//         echo "Your are playing at extreme level";
+//         break;
+//     case 5:
+//     default:
+//         echo "Invalid level";
+// }
+
+
+$var = 8;
+// while ($var <=10){
+//     echo "The number is: $var";
+//     $var++;
+// }
+
+// do{
+//     echo "number: $var ";
+//     $var++;
+// }while ($var <=10);
+
+function name(){
+    for($var = 0; $var <=10; $var++){
+        echo "The number is: $var\n";
+    }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+name();
+
+echo"i'm going to run this again later";
+name();
